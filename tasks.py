c66a040cc378413ada41dac60e5fdeefd1ff422f
asmoore <asmoor@gmail.com>
2015-02-12 14:40:54 -0500

    Create tasks.py
    Add a function to publish a message to Redis.

---
+++ tasks.py
@@ -0,0 +1,12 @@
+from chat import ChatBackend, app
+
+def pusblish_message(user, message):
+        chat = ChatBackend()
+        chat.subscribe('chat')
+        message = "{'handle': user, 'text': message}"
+        chat.publish(message)
+
+        
+if __name__ == '__main__': 
+    if '-publish_message' in sys.argv: 
+        publish_message('test user', 'test message') 
